4ee68219b680570010a2696577d469b51dde2c3d
Lucas Celestino <lucas.celp1@gmail.com>
2023-04-19 22:13:27 -0300

    teste cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import './shared/services/tradution'
 import {router} from './routes'
 import 'dotenv/config'
-
+const cors = require('cors')
 const server = express()
-
+server.use(cors)
 server.use(express.json())
 server.use(router)
 
